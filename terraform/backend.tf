7e04d6ad48d4cc393d120ccf0b54db1be699ec7a
aggosistemas <125746491+aggosistemas@users.noreply.github.com>
2025-01-01 17:56:53 -0300

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket-tfstate-toinho"
-    key            = "idp/infra/projectjava/terraform.tfstate"
-    region         = "ca-central-1"
-  }
-}
